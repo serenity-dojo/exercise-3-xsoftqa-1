9bb6c5ae24d0858fb8298f075aef4aaac49c6242
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-01 08:44:13 +0100

    Starting point

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -4,11 +4,4 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
-    @Test
-    public void creating_a_dog() {
-        Dog fido = new Dog("Fido","Bone", 5);
-        Assert.assertEquals(fido.getName(), "Fido");
-        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
-        Assert.assertEquals(fido.getAge(), 5);
-    }
 }
