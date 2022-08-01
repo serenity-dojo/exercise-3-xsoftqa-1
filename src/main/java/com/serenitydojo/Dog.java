817966f79c61d1a034bd8bb45363b19f7180ee2b
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-01 08:41:11 +0100

    Sample solution

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,31 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private String favoriteToy;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
